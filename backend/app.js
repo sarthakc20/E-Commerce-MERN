24726d7f4cb56ab06f4fdafd8ddf95baf5e7e0da
Sarthak Chatterjee <sarthatc@gmail.com>
2023-08-19 05:22:17 +0530

    Final Commit 3

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
 // Let Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
